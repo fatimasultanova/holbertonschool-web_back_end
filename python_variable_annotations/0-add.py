06aabf1ec43a224175bc16c45c83109b28bacf6a
Fatima Sultanova <fatimasultanova.909@gmail.com>
2026-06-12 14:09:55 +0400

    0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module provides basic arithmetic functions with type annotations.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Takes two float arguments and returns their sum as a float.
+
+    Parameters:
+    a (float): The first number to add.
+    b (float): The second number to add.
+
+    Returns:
+    float: The sum of a and b.
+    """
+    return a + b
