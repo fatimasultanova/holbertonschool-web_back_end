d8eae2f0b710abb83a2ea1b3fe94131cc3e3e572
Fatima Sultanova <fatimasultanova.909@gmail.com>
2026-08-12 15:02:17 +0400

    8

---
+++ NoSQL/8-all.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+List all documents in Python
+"""
+
+
+def list_all(mongo_collection):
+    """
+    Lists all documents in a pymongo collection object
+    """
+    return list(mongo_collection.find())
