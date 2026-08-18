e6f7bbccb926828f16387316d42c8018fab82ca0
Fatima Sultanova <fatimasultanova.909@gmail.com>
2026-08-18 22:44:03 +0400

    3

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,28 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  // Getters
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  // Setters
+  set code(value) {
+    this._code = value;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
