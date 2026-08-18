6d944898822222bae5c66e04f5591389f1132eab
Fatima Sultanova <fatimasultanova.909@gmail.com>
2026-08-18 22:46:42 +0400

    4

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,34 @@
+import Currency from './3-currency.js';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // Getters
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  // Setters
+  set amount(value) {
+    this._amount = value;
+  }
+
+  set currency(value) {
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
