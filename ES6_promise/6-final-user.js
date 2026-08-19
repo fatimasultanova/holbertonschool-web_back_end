f4d9a5102595de45fd8b4ede9b5e46f7ce5b333c
Fatima Sultanova <fatimasultanova.909@gmail.com>
2026-08-19 22:35:21 +0400

    6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,8 +2,13 @@ import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]);
+  const userPromise = signUpUser(firstName, lastName)
+    .then((value) => ({ status: 'fulfilled', value }))
+    .catch((error) => ({ status: 'rejected', value: error.toString() }));
+
+  const photoPromise = uploadPhoto(fileName)
+    .then((value) => ({ status: 'fulfilled', value }))
+    .catch((error) => ({ status: 'rejected', value: error.toString() }));
+
+  return Promise.all([userPromise, photoPromise]);
 }
